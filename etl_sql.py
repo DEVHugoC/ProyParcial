66f908dd11ab551a08c99137fde0886b17232c1d
Hugo Daniel <hugodanielcajacuri@gmail.com>
2025-04-12 17:43:00 -0500

    CAMBIOS SQL ETL

--- etl_sql.py
+++ etl_sql.py
@@ -1,8 +1,8 @@
 # Script de ingesta de SQL a Datalake
+
 import pandas as pd
 
 df = pd.read_csv('input.csv')
 df['processed'] = df['value'] * 100
 df.to_csv('output4.csv', index=False)
 print("ETL completado.")
-
